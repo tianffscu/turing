3816eb37867afbc5e6b754984692e674ac8bc37e
tianff <tianff.scu@gmail.com>
2018-04-18 22:54:42 +0800

    developing

---
+++ src/main/java/com/scu/turing/web/controller/IndexController.java
@@ -0,0 +1,66 @@
+package com.scu.turing.web.controller;
+
+import com.scu.turing.comm.Const;
+import com.scu.turing.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/")
+public class IndexController extends BaseController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+//        long size= collectRepository.countByUserIdAndIsDelete(getUserId(),IsDelete.NO);
+//        Config config = configRepository.findByUserId(getUserId());
+//        Favorites favorites = favoritesRepository.findOne(Long.parseLong(config.getDefaultFavorties()));
+//        List<String> followList = followRepository.findByUserId(getUserId());
+//        model.addAttribute("config",config);
+//        model.addAttribute("favorites",favorites);
+//        model.addAttribute("size",size);
+//        model.addAttribute("followList",followList);
+//        model.addAttribute("user",getUser());
+//        model.addAttribute("newAtMeCount",noticeRepository.countByUserIdAndTypeAndReaded(getUserId(), "at", "unread"));
+//        model.addAttribute("newCommentMeCount",noticeRepository.countByUserIdAndTypeAndReaded(getUserId(), "comment", "unread"));
+//        model.addAttribute("newPraiseMeCount",noticeRepository.countPraiseByUserIdAndReaded(getUserId(), "unread"));
+//        logger.info("collect size="+size+" userID="+getUserId());
+        return "home";
+    }
+
+    @GetMapping("/register")
+    public String regist() {
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletResponse response, Model model) {
+        getSession().removeAttribute(Const.LOGIN_SESSION_KEY);
+        getSession().removeAttribute(Const.LAST_REFERER);
+        Cookie cookie = new Cookie(Const.LOGIN_SESSION_KEY, "");
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String index(Model model){
+        model.addAttribute("collector","");
+        User user = getUser();
+        if(null != user){
+            model.addAttribute("user",user);
+        }
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/scu/turing/web/controller/UserController.java
@@ -0,0 +1,73 @@
+package com.scu.turing.web.controller;
+
+import com.scu.turing.comm.Const;
+import com.scu.turing.model.User;
+import com.scu.turing.model.result.ExceptionMsg;
+import com.scu.turing.model.result.Response;
+import com.scu.turing.model.result.ResponseData;
+import com.scu.turing.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController extends BaseController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(value = "/register")
+    public Response register(User user) {
+        try {
+            User registUser = userService.findByEmail(user.getEmail());
+            if (null != registUser) {
+                return result(ExceptionMsg.EmailUsed);
+            }
+
+            user.setPwd(getPwd(user.getPwd()));
+//            user.setCreateTime(DateUtils.getCurrentTime());
+//            user.setLastModifyTime(DateUtils.getCurrentTime());
+//            user.setProfilePicture("img/favicon.png");
+            userService.saveUser(user);
+            getSession().setAttribute(Const.LOGIN_SESSION_KEY, user);
+        } catch (Exception e) {
+            // TODO: handle exception
+            logger.error("create user failed, ", e);
+            return result(ExceptionMsg.FAILED);
+        }
+        return result();
+    }
+
+    @PostMapping(value = "/login")
+    public Response login(User user, HttpServletResponse response) {
+        try {
+            //这里不是bug，前端userName有可能是邮箱也有可能是昵称。
+            User loginUser = userService.findByEmail(user.getUserName());
+            if (loginUser == null) {
+                return new ResponseData(ExceptionMsg.LoginNameNotExists);
+            } else if (!loginUser.getPwd().equals(getPwd(user.getPwd()))) {
+                return new ResponseData(ExceptionMsg.LoginNameOrPassWordError);
+            }
+            Cookie cookie = new Cookie(Const.LOGIN_SESSION_KEY, cookieSign("" + loginUser.getId()));
+            cookie.setMaxAge(Const.COOKIE_TIMEOUT);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+            getSession().setAttribute(Const.LOGIN_SESSION_KEY, loginUser);
+            String preUrl = "/";
+            if (null != getSession().getAttribute(Const.LAST_REFERER)) {
+                preUrl = String.valueOf(getSession().getAttribute(Const.LAST_REFERER));
+            }
+            return new ResponseData(ExceptionMsg.SUCCESS, preUrl);
+        } catch (Exception e) {
+            // TODO: handle exception
+            logger.error("login failed, ", e);
+            return new ResponseData(ExceptionMsg.FAILED);
+        }
+    }
+}
